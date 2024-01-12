fe3d95a1f19ef5aaa9ddca95cd332fcc562ca324
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-01-13 01:31:23 +0200

    +

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from plyer import notification
 from datetime import datetime, date
 from tkinter import PhotoImage
 import datetime
+from tkinter import ttk
 
 
 class DateEntry(sd.Dialog):
@@ -28,6 +29,10 @@ def load_tasks():
 
 def add_task():
     task = simpledialog.askstring("Введіть завдання", "Назва завдання:")
+    if task and len(task) > 150:
+        messagebox.showwarning("Попередження", "Довжина завдання не може перевищувати 30 символів.")
+        return  # Повертаємось з функції, не додаючи завдання
+
     if task:
         priority = simpledialog.askinteger("Пріоритет", "Встановіть пріоритет (1-5):", minvalue=1, maxvalue=5)
         deadline = DateEntry(root).result
@@ -35,6 +40,8 @@ def add_task():
             formatted_task = f"[Пріоритет: {priority}] {task} (дедлайн: {deadline})"
             todo_list.add_task(formatted_task)
             update_tasks_display()
+            save_tasks()
+
 
 
 def edit_task():
@@ -111,10 +118,12 @@ def on_closing():
     root.destroy()
 
 
-def filter_tasks():
-    priority = simpledialog.askstring("Фільтр завдань за пріоритетом", "Введіть пріоритет (наприклад, 3):")
-    if priority:
-        filtered_tasks = [task for task in todo_list.tasks if f"[Пріоритет: {priority.strip()}]" in task]
+def filter_tasks(event):
+    selected_priority = dropdown.get()
+    if selected_priority == "Всі":
+        update_tasks_display()
+    else:
+        filtered_tasks = [task for task in todo_list.tasks if f"[Пріоритет: {selected_priority}]" in task]
         listbox.delete(0, tk.END)
         for task in filtered_tasks:
             listbox.insert(tk.END, task)
@@ -139,12 +148,23 @@ def check_for_notifications():
                 )
 
 
+
 def main():
-    global listbox, todo_list, root
+    global listbox, todo_list, root, dropdown
     todo_list = ToDoList()
     load_tasks()
 
     root = tk.Tk()
+
+    # зміна вікна на фоновий режим для відображення фонуваних елементів
+    # root.overrideredirect(True)
+
+    # root.attributes("-fullscreen", True)
+
+    # root.withdraw()
+    # востановлення вікна з фонового режиму
+    # root.lift()
+
     root.title("ToDo List")
 
     # Додавання міток та вдосконалення розташування
@@ -152,11 +172,11 @@ def main():
     frame.pack(pady=10)
 
     tk.Label(frame, text="Ваші завдання:").pack(side=tk.TOP)
-    listbox = tk.Listbox(frame, height=10, width=50)
+    listbox = tk.Listbox(frame, height=10, width=150, font=("Arial", 18))
     listbox.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     btn_frame = tk.Frame(root)
-    btn_frame.pack(pady=10)
+    btn_frame.pack(side=tk.LEFT, pady=10)
 
     add_icon = PhotoImage(file="img/add_task.png")
     edit_icon = PhotoImage(file="img/edit_task.png")
@@ -167,16 +187,20 @@ def main():
     tk.Button(btn_frame, image=delete_icon, command=remove_task).pack(side=tk.LEFT)
 
     tk.Button(root, text="Зберегти завдання", command=save_tasks).pack(pady=5)
-    tk.Button(root, text="Фільтр завдань", command=filter_tasks).pack(pady=5)
-    tk.Button(root, text="Показати всі завдання", command=show_all_tasks).pack(pady=5)
 
-    search_button = tk.Button(root, text="Пошук завдань", command=search_tasks)
+    label = tk.Label(btn_frame, text="Фільтр завдань за пріоритетом:")
+    label.pack(pady=(0, 0))
+    dropdown = ttk.Combobox(btn_frame, height=10, width=10, values=["Всі", "1", "2", "3", "4", "5"])
+    dropdown.pack(pady=5)
+    dropdown.bind("<<ComboboxSelected>>", filter_tasks)
+    dropdown.set("Всі")
+    tk.Button(btn_frame, text="Показати всі завдання", command=show_all_tasks).pack(pady=5)
+
+    search_button = tk.Button(btn_frame, text="Пошук завдань", command=search_tasks)
     search_button.pack(pady=5)
-
-    root.protocol("WM_DELETE_WINDOW", on_closing)
-
     update_tasks_display()
     check_for_notifications()
+    root.protocol("WM_DELETE_WINDOW", on_closing)
 
     root.mainloop()
 
